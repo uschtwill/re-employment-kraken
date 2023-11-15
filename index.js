4f7965686d0527aefe5d6567ebc4ba2723d367b2
Bastian Isensee <mail@bastian-isensee.com>
2023-11-15 13:11:04 +0100

    refactor: replace text files with SQLite based persistence
    * Add sqlite3 dependency
    * Create module for DB access (open, close, insertNew)
    * Create job table with unique url column and index for scalable
      deduplication
    * Implement persistence and detection of new jobs by url via DB insert
      statement
    * Use in-memory DB when DATABASE_ENABLED is set to false
    * Add test coverage for DB operations (add mocha dependency)
    * Update README file section about persistence
    * Test integration of DB manually:
      * New jobs are stored and notifications are sent
      * Existing jobs are skipped and notifications are not sent
      * In-memory settings deduplicated but avoid persistence

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import { config } from "./config.js"
 import { fetch } from "./lib/fetch.js"
-import { process } from "./lib/process.js"
+import { process as processStrategy } from "./lib/process.js"
+import { initDB, closeDB, DB_PERSISTENT, DB_IN_MEMORY } from "./lib/db.js"
 
-const init = async (config) => {
+const scrape = async (config) => {
   const { mode, scrapingStrategies, queries } = config
   if (mode === "sequential") {
     for (const strategy of scrapingStrategies) {
       for (const query of queries) {
         const htmlDocuments = await fetch(strategy, query, config)
-        await process(htmlDocuments, strategy, query, config)
+        await processStrategy(htmlDocuments, strategy, query, config)
       }
     }
   } else if (mode === "parallel") {
@@ -16,4 +17,16 @@ const init = async (config) => {
   }
 }
 
-init(config)
+process.on('unhandledRejection', (reason) => {
+  console.error('An unhandled Promise rejection occurred:', reason)
+})
+
+try {
+  // In case the database flag is turned off, we fall back to an in-memory DB for temporary deduplication.
+  await initDB(config.database.enabled ? DB_PERSISTENT : DB_IN_MEMORY)
+  await scrape(config)
+} catch (err) {
+  console.error(`An unexpected error occurred: ${err.message}`)
+} finally {
+  await closeDB()
+}
